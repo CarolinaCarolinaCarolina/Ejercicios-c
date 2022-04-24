57f9add917d165a91833c11313ddf7ff4c7d1c2a
Santiago Suarez <Santiago32296@gmail.com>
2022-04-24 16:49:21 -0500

    Se agregó el ejercisio de numeros primos

---
+++ Ejercicios  c/Parte 3/Ejer3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios__c.Parte_3
+{
+    internal class Ejer3
+    {
+        public void Ejecucion()
+        {
+            Console.WriteLine("Ingrese el numero del ejercisio");
+            int numero = int.Parse(Console.ReadLine());
+            do
+            {
+                switch (numero)
+                {
+                    case 1:
+                        Ejer1();
+                        break;
+                    case 2:
+                        //Ejer2();
+                        break;
+                    case 3:
+                        //Ejer3();
+                        break;
+                    case 4:
+                        //Ejer4();
+                        break;
+                }
+            } while (numero != 4);
+        }
+        public static void Ejer1()
+        {
+            Console.WriteLine("Ingrese el numero");
+            int numero = int.Parse(Console.ReadLine());
+            int cont = 0;
+            for (int i = 2; i <= numero; i++)
+            {  //apertura del for1
+                for (int j = 1; j <= i; j++)
+                {   //apertura for2
+
+                    if (i % j == 0)
+                    {
+                        cont = cont + 1;
+                    }
+                }
+
+
+                if (cont <= 2)
+                { //si variable cont es mayor a dos, el numero es divisible en mas de dos numero osea no es primo
+                    Console.WriteLine(i);
+                }      //cierre for2
+                cont = 0;               //igualamos a cero para hacer otra evaluacion con otro numero
+
+
+            }//cierre del for1
+
+        }
+    }
+}
--- Ejercicios  c/Program.cs
+++ Ejercicios  c/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Ejercicios__c.Parte_2;
+using Ejercicios__c.Parte_3;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,10 @@ namespace Ejercicios__c
                     case 2:
                         Ejer e1 = new Ejer();
                         e1.Ejecucion();
+                        break;
+                    case 3:
+                        Ejer3 e3 = new Ejer3();
+                        e3.Ejecucion();
                         break;
                 }
             } while (opcion != 3);
